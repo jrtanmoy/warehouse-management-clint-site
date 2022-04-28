72f8a698fb307ca17ae6ddc9280890b2dc42834a
jrtanmoy <roy28301@gmail.com>
2022-04-28 21:34:52 +0600

    install & set react router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Home from './components/Home/Home';
+import About from './components/About/About';
+import Navber from './components/Navber/Navber';
 
 function App() {
   return (
     <div>
-      <h1>checking connention.</h1>
+      <Navber></Navber>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </div>
   );
 }
