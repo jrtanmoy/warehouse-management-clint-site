609fd98cbf739e5025e242dacd39686eb43b819a
jrtanmoy <roy28301@gmail.com>
2022-04-29 08:33:43 +0600

    i firebase & secure config by using env file

--- src/components/Navber/Navber.js
+++ src/components/Navber/Navber.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Container, Nav, Navbar, } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
-import logo from '../../images/grocery-logo1.png'
+import logo from '../../images/warehouse-logo2 .png'
 
 
 
@@ -12,7 +12,7 @@ const Navber = () => {
             <Navbar className='container' collapseOnSelect expand="lg" sticky='top' bg="success" variant="dark">
                 <Container>
                     <Navbar.Brand as={Link} to="/">
-                        <img height={45} src={logo} alt="" />
+                        <img height={60} src={logo} alt="" />
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                     <Navbar.Collapse id="responsive-navbar-nav">
