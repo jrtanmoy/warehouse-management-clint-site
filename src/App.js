a53c4768675f7499ca143508e436c51fb1cf08ed
jrtanmoy <roy28301@gmail.com>
2022-04-29 09:36:53 +0600

    checked signup link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Navber from './components/Navber/Navber';
 import Blogs from './components/Blogs/Blogs';
 import NotFound from './components/NotFound/NotFound';
 import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
 
 function App() {
   return (
@@ -15,6 +16,7 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/blogs" element={<Blogs />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
