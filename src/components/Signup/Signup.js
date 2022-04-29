dd606a9ea71108af69644fca694327b61cb5dd1a
jrtanmoy <roy28301@gmail.com>
2022-04-29 10:14:34 +0600

    signup functionality added

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -1,9 +1,125 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import { useCreateUserWithEmailAndPassword } from "react-firebase-hooks/auth";
+import { useLocation, useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { auth } from "../../firebase.init";
+
+import "../Login/Login.css";
 
 const Signup = () => {
+    const [userInfo, setUserInfo] = useState({
+        email: "",
+        password: "",
+        confirmPass: "",
+    });
+    const [errors, setErrors] = useState({
+        email: "",
+        password: "",
+        general: "",
+    });
+
+    const [showPass, setShowPass] = useState(false);
+
+    const [createUserWithEmailAndPassword, user, loading, hookError] =
+        useCreateUserWithEmailAndPassword(auth, { sendEmailVerification: true });
+
+    const handleEmailChange = (e) => {
+        const emailRegex = /\S+@\S+\.\S+/;
+        const validEmail = emailRegex.test(e.target.value);
+
+        if (validEmail) {
+            setUserInfo({ ...userInfo, email: e.target.value });
+            setErrors({ ...errors, email: "" });
+        } else {
+            setErrors({ ...errors, email: "Invalid email" });
+            setUserInfo({ ...userInfo, email: "" });
+        }
+
+        // setEmail(e.target.value);
+    };
+    const handlePasswordChange = (e) => {
+        const passwordRegex = /.{6,}/;
+        const validPassword = passwordRegex.test(e.target.value);
+
+        if (validPassword) {
+            setUserInfo({ ...userInfo, password: e.target.value });
+            setErrors({ ...errors, password: "" });
+        } else {
+            setErrors({ ...errors, password: "Minimum 6 characters!" });
+            setUserInfo({ ...userInfo, password: "" });
+        }
+    };
+
+    const handleConfirmPasswordChange = (e) => {
+        if (e.target.value === userInfo.password) {
+            setUserInfo({ ...userInfo, confirmPass: e.target.value });
+            setErrors({ ...errors, password: "" });
+        } else {
+            setErrors({ ...errors, password: "Password's don't match" });
+            setUserInfo({ ...userInfo, confirmPass: "" });
+        }
+    };
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        console.log(userInfo);
+        createUserWithEmailAndPassword(userInfo.email, userInfo.password);
+    };
+
+    useEffect(() => {
+        if (hookError) {
+            switch (hookError?.code) {
+                case "auth/invalid-email":
+                    toast("Invalid email provided, please provide a valid email");
+                    break;
+                case "auth/invalid-password":
+                    toast("Wrong password. Intruder!!");
+                    break;
+                default:
+                    toast("something went wrong");
+            }
+        }
+    }, [hookError]);
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || "/";
+
+    useEffect(() => {
+        if (user) {
+            navigate(from);
+        }
+    }, [user]);
+
     return (
-        <div>
-            <h1>Signup</h1>
+        <div className="login-container">
+            <div className="login-title">Sign up</div>
+            <form className="login-form" onSubmit={handleLogin}>
+                <input type="text" placeholder="Your Email" onChange={handleEmailChange} required />
+                {errors?.email && <p className="error-message">{errors.email}</p>}
+                <div className="relative">
+                    <input type={showPass ? "text" : "password"} placeholder="password" onChange={handlePasswordChange} required />
+
+                    <p className="position-relative" onClick={() => setShowPass(!showPass)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" className="position-absolute top-50 end-0 translate-middle-y w-25 mb-5 pb-5 pe-4 me-1 mt-0 pt-0" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
+                        </svg>
+                    </p>
+                    {errors?.password && <p className="error-message">{errors.password}</p>}
+                </div>
+                <input
+                    type="password"
+                    placeholder="confirm password"
+                    onChange={handleConfirmPasswordChange}
+                    required
+                />
+
+                <button>Sign up</button>
+
+                <ToastContainer />
+            </form>
         </div>
     );
 };
