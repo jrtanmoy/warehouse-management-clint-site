737f602724253c9a698b9e67d21745413b7b653b
jrtanmoy <roy28301@gmail.com>
2022-04-29 09:17:57 +0600

    login functionality added

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,9 +1,127 @@
-import React from 'react';
+import { useEffect, useState } from "react";
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword, useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { Link, useLocation, useNavigate } from "react-router-dom";
+import { toast, ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { auth } from "../../firebase.init";
+import googlelogo from '../../images/google.svg'
+import "./Login.css";
+
 
 const Login = () => {
+    const [userInfo, setUserInfo] = useState({
+        email: "",
+        password: "",
+    })
+    const [errors, setErrors] = useState({
+        email: "",
+        password: "",
+        general: "",
+    })
+
+    const [signInWithEmail, user, loading, hookError] = useSignInWithEmailAndPassword(auth);
+    const [signInWithGoogle, googleUser, loading2, googleError] = useSignInWithGoogle(auth);
+
+    const [sendPasswordResetEmail, sending] = useSendPasswordResetEmail(auth);
+
+    const handleEmailChange = (e) => {
+        const emailRegex = /\S+@\S+\.\S+/;
+        const validEmail = emailRegex.test(e.target.value);
+
+        if (validEmail) {
+            setUserInfo({ ...userInfo, email: e.target.value })
+            setErrors({ ...errors, email: "" })
+        } else {
+            setErrors({ ...errors, email: "Invalid email" })
+            setUserInfo({ ...userInfo, email: "" })
+        }
+
+
+
+        // setEmail(e.target.value);
+    }
+    const handlePasswordChange = (e) => {
+        const passwordRegex = /.{6,}/;
+        const validPassword = passwordRegex.test(e.target.value);
+
+        if (validPassword) {
+            setUserInfo({ ...userInfo, password: e.target.value });
+            setErrors({ ...errors, password: "" });
+        } else {
+            setErrors({ ...errors, password: "Minimum 6 characters!" });
+            setUserInfo({ ...userInfo, password: "" })
+        }
+
+    }
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        console.log(userInfo)
+
+        signInWithEmail(userInfo.email, userInfo.password);
+
+    }
+
+    const navigate = useNavigate();
+    const location = useLocation();
+    const from = location.state?.from?.pathname || "/";
+
+    useEffect(() => {
+        if (user || googleUser) {
+            navigate(from);
+        }
+    }, [user, googleUser]);
+
+    useEffect(() => {
+        const error = hookError || googleError;
+        if (error) {
+            switch (error?.code) {
+                case "auth/invalid-email":
+                    toast("Invalid email provided, please provide a valid email");
+                    break;
+
+                case "auth/invalid-password":
+                    toast("Wrong password. Intruder!!")
+                    break;
+                default:
+                    toast("something went wrong")
+            }
+        }
+    }, [hookError, googleError])
+
+    const resetPassword = async (e) => {
+        const email = userInfo.email
+        if (email) {
+            await sendPasswordResetEmail(email);
+            toast('Sent email');
+        }
+        else {
+            toast('please enter your email address');
+        }
+    }
+
     return (
-        <div>
-            <h1>This is login.</h1>
+        <div className="login-container">
+            <div className="login-title">LOGIN</div>
+            <form className="login-form" onSubmit={handleLogin}>
+                <input type="text" placeholder="Your Email" onChange={handleEmailChange} required />
+                {errors?.email && <p className="error-message">{errors.email}</p>}
+                <input type="password" placeholder="password" onChange={handlePasswordChange} required />
+                {errors?.password && <p className="error-message">{errors.password}</p>}
+                <button>Login</button>
+
+                <ToastContainer />
+
+                <p className="mt-3">Don't have an account? <Link to="/signup">Sign up first</Link> </p>
+                <p>Forget Password? <button className='btn btn-link text-white pe-auto text-decoration-none' onClick={resetPassword}>Reset Password</button> </p>
+            </form>
+            <p>Log in with one of the following:</p>
+
+            <button className="bg-dark" onClick={() => signInWithGoogle()}>
+                <img src={googlelogo} alt="google logo" />
+                <span className="px-2">Google</span>
+            </button>
         </div>
     );
 };
