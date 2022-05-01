7fe79438e5cf545ca54ecc91af3ac31e45d3adad
jrtanmoy <roy28301@gmail.com>
2022-05-01 12:34:43 +0600

    add item functionality done

--- src/components/AddNewItem/AddNewItem.js
+++ src/components/AddNewItem/AddNewItem.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
 
 const AddNewItem = () => {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = data => {
+        console.log(data);
+        const url = `http://localhost:5000/inventory`;
+        fetch(url, {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(res => res.json())
+            .then(result => {
+                console.log(result);
+            })
+    };
+
+
     return (
-        <div>
-            <h1>Add New Item.</h1>
+        <div className='w-50 mx-auto mt-4'>
+            <h1>Please add new item.</h1>
+            <form className='d-flex flex-column' onSubmit={handleSubmit(onSubmit)}>
+                <input className='mb-2' placeholder='Name' {...register("name", { required: true, maxLength: 20 })} />
+                <input className='mb-2' placeholder='Supplier Name' {...register("supplierName", { required: true, maxLength: 20 })} />
+                <textarea className='mb-2' placeholder='Description' {...register("description")} />
+                <input className='mb-2' placeholder='Price' type="number" {...register("price")} />
+                <input className='mb-2' placeholder='Quantity' type="number" {...register("quantity")} />
+                <input className='mb-2' placeholder='Photo URL' type="text" {...register("img")} />
+                <input type="submit" value="Add Service" />
+            </form>
         </div>
     );
 };
