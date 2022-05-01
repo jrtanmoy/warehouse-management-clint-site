b96e941f308a5253a63a55d269fefea101e3f7b8
jrtanmoy <roy28301@gmail.com>
2022-05-01 21:07:18 +0600

    restock quantity done

--- src/components/InventoryDetail/InventoryDetail.js
+++ src/components/InventoryDetail/InventoryDetail.js
@@ -5,6 +5,7 @@ import '../Inventory/Inventory.css'
 const InventoryDetail = () => {
     const { inventoryId } = useParams();
     const [inventory, setInventroy] = useState({});
+    
 
     useEffect(() => {
         const url = `http://localhost:5000/inventory/${inventoryId}`
@@ -13,6 +14,35 @@ const InventoryDetail = () => {
             .then(res => res.json())
             .then(data => setInventroy(data));
     })
+
+    const handleUpdateRestock = event =>{
+        const { _id, name, supplierName, img, description, price, quantity } = inventory;
+        const oldquantity = parseInt(quantity);
+        console.log(oldquantity);
+        event.preventDefault();
+        const restockquantity = parseInt(event.target.quantity.value);
+        console.log(restockquantity);
+        const totalQuantity = oldquantity+ restockquantity;
+
+        const updatedRestock = {totalQuantity};
+
+        // send data to the server
+        const url = `http://localhost:5000/inventory/${inventoryId}`;
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(updatedRestock)
+        })
+        .then(res => res.json())
+        .then(data =>{
+            console.log('success', data);
+            alert('Restock the item quantity successfully!!!');
+            event.target.reset();
+        })
+    }
+
     return (
         <div className='container'>
             <h2 className='text-center  mt-4 mb-4'>Welcome to detail</h2>
@@ -30,13 +60,23 @@ const InventoryDetail = () => {
 
                 <div className='col text-center mt-4 mb-5 '>
                     <div className='mb-4'>
+                        <form onSubmit={handleUpdateRestock}>
+                            <input className='mb-2' type="number" name="quantity" placeholder='Quantity' required />
+                            <br />
+                            <input type="submit" value="Restock the items" />
+                        </form>
+                    </div>
+                    <div className='mb-4'>
+                        <button className='btn btn-primary w-25 mx-auto'>Delivered</button>
+                    </div>
+                    <div className='mb-4'>
                         <Link to='/checkout'>
-                            <button className='btn btn-primary'>Proceed Checkout</button>
+                            <button className='btn btn-primary w-25 mx-auto'>Proceed Checkout</button>
                         </Link>
                     </div>
                     <div>
                         <Link to='/manageinventories'>
-                            <button className='btn btn-primary'>Manage Inventories</button>
+                            <button className='btn btn-primary w-25 mx-auto'>Manage Inventories</button>
                         </Link>
                     </div>
                 </div>
