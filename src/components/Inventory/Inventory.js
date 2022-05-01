c109f3cf4d92b9e39adad2f5a9b5456b9fad1621
jrtanmoy <roy28301@gmail.com>
2022-05-01 13:59:27 +0600

    update some page

--- src/components/Inventory/Inventory.js
+++ src/components/Inventory/Inventory.js
@@ -15,8 +15,8 @@ const Inventory = ({ inventory }) => {
             <div className='inventory-info mt-4'>
                 <h2 className='text-primary'>{name}</h2>
                 <h4>Supplier Name: {supplierName}</h4>
-                <p>Price: ${price}</p>
-                <p>Quantity: {quantity}</p>
+                <p>Price: ${price}/Container</p>
+                <p>Quantity: {quantity} Container</p>
                 <p><small>{description}</small></p>
             </div>
             <button onClick={() => navigateToInventoryDetail(_id)} className='btn-cart'>Update</button>
--- src/components/ManageInventory/ManageInventory.js
+++ src/components/ManageInventory/ManageInventory.js
@@ -30,8 +30,8 @@ const ManageInventory = ({ inventory, ui }) => {
         <div className='inventory-info mt-4'>
             <h2 className='text-primary'>{name}</h2>
             <h4>Supplier Name: {supplierName}</h4>
-            <p>Price: ${price}</p>
-            <p>Quantity: {quantity}</p>
+            <p>Price: ${price}/Container</p>
+            <p>Quantity: {quantity} Container</p>
             <p><small>{description}</small></p>
         </div>
         <button onClick={() => handleDelete(_id)} className='btn-cart'>Delete</button>
