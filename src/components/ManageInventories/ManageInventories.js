6abf615c2b48fc36fa34c0d2e569f6f7007dead3
jrtanmoy <roy28301@gmail.com>
2022-05-01 09:23:10 +0600

    added manageinventory & update manageinventories

--- src/components/ManageInventories/ManageInventories.js
+++ src/components/ManageInventories/ManageInventories.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import useInventories from '../Hooks/useInventories';
+import ManageInventory from '../ManageInventory/ManageInventory';
+import '../Inventories/Inventories.css'
 
 const ManageInventories = () => {
+    const [inventories] = useInventories([]);
     return (
-        <div>
-            <h1>ManageInventories</h1>
+        <div id="inventories" className='container'>
+            <div className="row">
+                <h1 className='text-success text-center mt-5 mb-5'>Inventory Items</h1>
+                <div className="p-2 gap-5 inventories-container">
+                    {
+                        inventories.map(inventory => <ManageInventory
+                            key={inventory._id}
+                            inventory={inventory}
+                        >
+                        </ManageInventory>)
+                    }
+                </div>
+            </div>
         </div>
     );
 };
---
+++ src/components/ManageInventory/ManageInventory.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../Inventory/Inventory.css';
+
+const ManageInventory = ({ inventory }) => {
+    console.log(inventory);
+    const { _id, name, supplierName, img, description, price, quantity } = inventory;
+    const navigate = useNavigate();
+    const navigateToInventoryDetail = id => {
+        navigate(`/inventory/${id}`);
+    }
+    return (
+        <div className='inventory'>
+        <img className='img-fluid' src={img} alt="" />
+        <div className='inventory-info mt-4'>
+            <h2 className='text-primary'>{name}</h2>
+            <h4>Supplier Name: {supplierName}</h4>
+            <p>Price: ${price}</p>
+            <p>Quantity: {quantity}</p>
+            <p><small>{description}</small></p>
+        </div>
+        <button onClick={() => navigateToInventoryDetail(_id)} className='btn-cart'>Delete</button>
+    </div>
+    );
+};
+
+export default ManageInventory;
