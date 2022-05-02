624cfb296cdad06b7c5323c7795d80fd3a05a38a
jrtanmoy <roy28301@gmail.com>
2022-05-02 10:00:11 +0600

    edit inventorydetail design

--- src/components/InventoryDetail/InventoryDetail.js
+++ src/components/InventoryDetail/InventoryDetail.js
@@ -1,11 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import '../Inventory/Inventory.css'
+import './InventoryDetail.css'
 
 const InventoryDetail = () => {
     const { inventoryId } = useParams();
     const [inventory, setInventroy] = useState({});
-    
+
 
     useEffect(() => {
         const url = `http://localhost:5000/inventory/${inventoryId}`
@@ -15,14 +16,14 @@ const InventoryDetail = () => {
             .then(data => setInventroy(data));
     })
 
-    const handleUpdateRestock = event =>{
+    const handleUpdateRestock = event => {
         event.preventDefault();
         const { quantity } = inventory;
         const oldquantity = parseInt(quantity);
         const restockquantity = parseInt(event.target.quantity.value);
-        const totalQuantity = oldquantity+ restockquantity;
+        const totalQuantity = oldquantity + restockquantity;
 
-        const updatedStock = {totalQuantity};
+        const updatedStock = { totalQuantity };
 
         // send data to the server
         const url = `http://localhost:5000/inventory/${inventoryId}`;
@@ -33,22 +34,22 @@ const InventoryDetail = () => {
             },
             body: JSON.stringify(updatedStock)
         })
-        .then(res => res.json())
-        .then(data =>{
-            console.log('success', data);
-            alert('Restock the item quantity successfully!!!');
-            event.target.reset();
-        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('success', data);
+                alert('Restock the item quantity successfully!!!');
+                event.target.reset();
+            })
     }
 
-    const handleUpdateDelivered = () =>{
-        
+    const handleUpdateDelivered = () => {
+
         const { quantity } = inventory;
         const oldquantity = parseInt(quantity);
-        
+
         const totalQuantity = oldquantity - 1;
 
-        const updatedStock = {totalQuantity};
+        const updatedStock = { totalQuantity };
 
         // send data to the server
         const url = `http://localhost:5000/inventory/${inventoryId}`;
@@ -59,30 +60,33 @@ const InventoryDetail = () => {
             },
             body: JSON.stringify(updatedStock)
         })
-        .then(res => res.json())
-        .then(data =>{
-            console.log('success', data);
-            alert('Delivered successfully!!!');
-        })
+            .then(res => res.json())
+            .then(data => {
+                console.log('success', data);
+                alert('Delivered successfully!!!');
+            })
     }
 
 
     return (
         <div className='container'>
             <h2 className='text-center  mt-4 mb-4'>Welcome to detail</h2>
-            <div className='row gx-2 d-flex align-items-center justify-content-center'>
-                <div className='col inventory'>
-                    <img className='img-fluid' src={inventory.img} alt="" />
-                    <div className='inventory-info mt-4'>
-                        <h2 className='text-primary'>{inventory.name}</h2>
-                        <h4>Supplier Name: {inventory.supplierName}</h4>
-                        <p>Price: ${inventory.price}</p>
-                        <p>Quantity: {inventory.quantity}</p>
-                        <p><small>{inventory.description}</small></p>
+            <div className='inventoryDetail-container'>
+                
+                <div className='d-flex align-items-center justify-content-center'>
+                    <div className='inventory'>
+                        <img className='img-fluid' src={inventory.img} alt="" />
+                        <div className='inventory-info mt-4'>
+                            <h2 className='text-primary'>{inventory.name}</h2>
+                            <h4>Supplier Name: {inventory.supplierName}</h4>
+                            <p>Price: ${inventory.price}</p>
+                            <p>Quantity: {inventory.quantity}</p>
+                            <p><small>{inventory.description}</small></p>
+                        </div>
                     </div>
                 </div>
 
-                <div className='col text-center mt-4 mb-5 '>
+                <div className=' text-center mt-4 mb-5 '>
                     <div className='mb-4'>
                         <form onSubmit={handleUpdateRestock}>
                             <h5>Restock the items</h5>
@@ -92,19 +96,20 @@ const InventoryDetail = () => {
                         </form>
                     </div>
                     <div className='mb-4'>
-                        <button onClick={handleUpdateDelivered} className='btn btn-primary w-25 mx-auto'>Delivered</button>
+                        <button onClick={handleUpdateDelivered} className='btn btn-primary w-50 mx-auto'>Delivered</button>
                     </div>
                     <div className='mb-4'>
                         <Link to='/checkout'>
-                            <button className='btn btn-primary w-25 mx-auto'>Proceed Checkout</button>
+                            <button className='btn btn-primary w-50 mx-auto'>Proceed Checkout</button>
                         </Link>
                     </div>
                     <div>
                         <Link to='/manageinventories'>
-                            <button className='btn btn-primary w-25 mx-auto'>Manage Inventories</button>
+                            <button className='btn btn-primary w-50 mx-auto'>Manage Inventories</button>
                         </Link>
                     </div>
                 </div>
+
             </div>
         </div>
     );
