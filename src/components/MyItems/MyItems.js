b94e92ca40e9b538bae17a88bae1156a170fd991
jrtanmoy <roy28301@gmail.com>
2022-05-02 17:13:23 +0600

    update functionaliy for myItems page

--- src/components/MyItems/MyItems.js
+++ src/components/MyItems/MyItems.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import { auth } from '../../firebase.init';
 
 const MyItems = () => {
+    const [user] = useAuthState(auth);
+    const [myItems, setMyItems] = useState([])
+    useEffect(() => {
+        const getMyItems = async () => {
+            const email = user.email;
+            const url = `http://localhost:5000/myItem?email=${email}`;
+            const { data } = await axios.get(url);
+            setMyItems(data);
+        }
+        getMyItems();
+
+    }, [user])
     return (
         <div>
-            Your orders
+            <h1>Your items: {myItems.length}</h1>
         </div>
     );
 };
